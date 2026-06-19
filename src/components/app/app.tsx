72641ae33a023ef1ee72841161548543d69aa897
VladissGernet <skeimorr@gmail.com>
2026-06-19 13:54:47 +0400

    WIP: остановился на описании типа предложений

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import MainScreen from '../../pages/main-screen/main-screen';
 
-export default function App(): JSX.Element {
-  return <MainScreen />;
+export default function App(offers): JSX.Element {
+  // TODO описать тип offers
+  return <MainScreen offers={offers} />;
 }
---
+++ src/mocks/offers.ts
@@ -0,0 +1,94 @@
+export const offers = [
+  {
+    id: '9b1ecd0c-6178-4b4b-90a3-9d49a9727e5c',
+    title: 'The Joshua Tree House',
+    type: 'apartment',
+    price: 296,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.868610000000004,
+      longitude: 2.342499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 3.9,
+  },
+  {
+    id: '4a76f6b8-c4ff-42ec-a207-ff7c2ae0b294',
+    title: 'Canal View Prinsengracht',
+    type: 'house',
+    price: 790,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/19.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 2.7,
+  },
+  {
+    id: '2424b273-79e0-436f-9cde-8dabec40fd91',
+    title: 'House in countryside',
+    type: 'hotel',
+    price: 469,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/8.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 4.9,
+  },
+  {
+    id: '6bd7a6c8-8e61-4acd-9982-666adccece00',
+    title: 'Penthouse, 4-5 rooms + 5 balconies',
+    type: 'house',
+    price: 766,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.85761,
+      longitude: 2.358499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 1.5,
+  },
+];
