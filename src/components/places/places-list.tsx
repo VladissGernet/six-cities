203475126e2cc4c534e9ac10e96786275c8d5fd6
VladissGernet <skeimorr@gmail.com>
2026-06-19 18:38:53 +0400

    WIP: остановился на прокидывании данных в карточку.

--- src/components/places/places-list.tsx
+++ src/components/places/places-list.tsx
@@ -9,12 +9,16 @@ export default function PlacesList({ offers }: PlacesListProps): JSX.Element {
   console.log(offers);
 
   return (
+    // TODO, Остноавился на прокидывания данных в карточку
     <div className="cities__places-list places__list">
+      {offers.map((offer) => {
+        console.log(offer);
+      })}
+      {/* <PlaceCard />
       <PlaceCard />
       <PlaceCard />
       <PlaceCard />
-      <PlaceCard />
-      <PlaceCard />
+      <PlaceCard /> */}
     </div>
   );
 }
