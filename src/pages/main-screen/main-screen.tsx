41ee916a0b0c189e4aeb3aa1cefd1519ecedb370
VladissGernet <skeimorr@gmail.com>
2026-06-27 17:11:30 +0400

    Деструктуризировал main-screen

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -2,7 +2,8 @@ import { Offers } from '../../types/offers';
 import Page from '../../components/page/page';
 import Header from '../../components/header/header';
 import Main from '../../components/main/main';
-import Places from '../../components/places/places';
+import Tabs from '../../components/tabs/tabs';
+import Cities from '../../components/cities/cities';
 
 type MainScreenProps = {
   offers: Offers;
@@ -15,50 +16,8 @@ export default function MainScreen({ offers }: MainScreenProps): JSX.Element {
 
       <Main isIndex>
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <Places offers={offers} />
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
-          </div>
-        </div>
+        <Tabs />
+        <Cities offers={offers} />
       </Main>
     </Page>
   );
