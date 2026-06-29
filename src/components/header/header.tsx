ae705a150d524d21d6cddc25cc9099142dd53870
VladissGernet <skeimorr@gmail.com>
2026-06-29 12:10:44 +0400

    Дополнил состояния хедера

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,42 @@
-export default function Header(): JSX.Element {
+type HeaderProps = {
+  isLoggedIn?: boolean;
+};
+
+export default function Header({ isLoggedIn }: HeaderProps): JSX.Element {
+  const loggedInElement = (
+    <>
+      <li className="header__nav-item user">
+        <a className="header__nav-link header__nav-link--profile" href="#">
+          <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+          <span className="header__user-name user__name">
+            Oliver.conner@gmail.com
+          </span>
+          <span className="header__favorite-count">3</span>
+        </a>
+      </li>
+      <li className="header__nav-item">
+        <a className="header__nav-link" href="#">
+          <span className="header__signout">Sign out</span>
+        </a>
+      </li>
+    </>
+  );
+
+  const signInElement = (
+    <li className="header__nav-item user">
+      <a className="header__nav-link header__nav-link--profile" href="#">
+        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+        <span className="header__login">Sign in</span>
+      </a>
+    </li>
+  );
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
+            {/* TODO, на главной странице возможно придется деактивировать. */}
             <a className="header__logo-link header__logo-link--active">
               <img
                 className="header__logo"
@@ -16,23 +49,11 @@ export default function Header(): JSX.Element {
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a
-                  className="header__nav-link header__nav-link--profile"
-                  href="#"
-                >
-                  <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                  <span className="header__user-name user__name">
-                    Oliver.conner@gmail.com
-                  </span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {/*
+                TODO, рассмотреть рефакторинг в будущем с помощью React.memo и useMemo.
+                Данная реализация редерит выше сразу ДВЕ разметки для пустого и полного элемента.
+              */}
+              {isLoggedIn ? loggedInElement : signInElement}
             </ul>
           </nav>
         </div>
