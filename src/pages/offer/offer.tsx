9018d9cae70fc708d5e84d343a659b7b704d25fa
VladissGernet <skeimorr@gmail.com>
2026-07-01 14:26:47 +0400

    Разметка страницы offer

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -1,6 +1,8 @@
 import Page from '../../components/page/page';
 import Header from '../../components/header/header';
 import Main from '../../components/main/main';
+import Gallery from '../../components/gallery/gallery';
+// import Rating from '../../components/rating/rating';
 
 export default function Offer(): JSX.Element {
   // TODO, остановился здесь
@@ -10,50 +12,7 @@ export default function Offer(): JSX.Element {
       <Main isOffer>
         <section className="offer">
           <div className="offer__gallery-container container">
-            <div className="offer__gallery">
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/room.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-02.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-03.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/studio-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-            </div>
+            <Gallery />
           </div>
           <div className="offer__container container">
             <div className="offer__wrapper">
