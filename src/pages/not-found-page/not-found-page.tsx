ac13405e41d16d9240b14d25e8caed41d364d972
VladissGernet <skeimorr@gmail.com>
2026-07-13 15:36:37 +0400

    Добавил стилей на страничку 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -3,13 +3,16 @@ import Header from '../../components/header/header';
 import Main from '../../components/main/main';
 import Footer from '../../components/footer/footer';
 
+import cn from 'classnames';
+import styles from './not-found-page.module.css';
+
 export default function NotFoundPage(): JSX.Element {
   return (
     <Page>
       <Header />
 
       <Main>
-        <div className="container">
+        <div className={cn('container', styles['container'])}>
           <h1>404 Not Found</h1>
           <p>This page doesn’t exist.</p>
           {/* TODO, исправить на <Link> из реакт роутера */}
