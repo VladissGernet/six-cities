d2246e3be0e016905b1e2bc2dd01d2ba60c2e966
VladissGernet <skeimorr@gmail.com>
2026-08-08 13:07:21 +0400

    WIP: Добавил дефолтную карту

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 import cn from 'classnames';
 import { Offers } from '../../types/offers';
-import { Map, TileLayer } from 'leaflet';
+import { Map as LeafletMap, TileLayer } from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 type MapProps = {
   rootClassName: string;
@@ -11,6 +13,32 @@ type MapProps = {
 
 export default function Map({ rootClassName, offers }: MapProps): JSX.Element {
   console.log(offers);
+  const mapRef = useRef<HTMLElement | null>(null);
 
-  return <section className={cn(rootClassName, 'map')} />;
+  useEffect(() => {
+    if (mapRef.current === null) {
+      return;
+    }
+
+    const map = new LeafletMap(mapRef.current, {
+      center: [0, 0],
+      zoom: 10,
+    });
+
+    const layer = new TileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      },
+    );
+
+    map.addLayer(layer);
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <section className={cn(rootClassName, 'map')} ref={mapRef} />;
 }
