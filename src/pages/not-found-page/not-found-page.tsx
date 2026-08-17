dfefb5e90f49aaad9250a1d861faf8b09182ba09
VladissGernet <skeimorr@gmail.com>
2026-08-17 14:52:17 +0400

    Исправил ссылку перехода на главную.

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import Page from '../../components/page/page';
 import Header from '../../components/header/header';
 import Main from '../../components/main/main';
@@ -15,8 +18,7 @@ export default function NotFoundPage(): JSX.Element {
         <div className={cn('container', styles['container'])}>
           <h1>404 Not Found</h1>
           <p>This page doesn’t exist.</p>
-          {/* TODO, исправить на <Link> из реакт роутера */}
-          <a>Return to main page</a>
+          <Link to={AppRoute.Root}>Return to main page</Link>
         </div>
       </Main>
       <Footer />
