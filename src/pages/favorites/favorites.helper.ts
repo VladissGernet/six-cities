76063b23255fcad123e49a8e757808788a40ade0
VladissGernet <skeimorr@gmail.com>
2026-09-20 12:29:11 +0400

    Добавил заметку

--- src/pages/favorites/favorites.helper.ts
+++ src/pages/favorites/favorites.helper.ts
@@ -13,7 +13,32 @@ import type { GroupedOffers, GroupedOffersByCity } from '../../types/offers';
 function filterFavoriteOffers(
   groupedOffers: GroupedOffers,
 ): GroupedOffersByCity[] {
-  // TODO исправить типизацию
+  /*
+  TODO исправить ошбику фильтрации и типизацию
+
+  Почему forEach не работает
+    У типа Partial<Record<CityName, Offers>> нет метода forEach. forEach есть у:
+
+    массивов (Offer[]);
+
+    Map, Set и некоторых коллекций.
+
+    А у обычного объекта его нет. Поэтому, когда ты пишешь:
+
+    ts
+    groupedOffers.forEach((offers, city) => {
+    TypeScript видит:
+
+    groupedOffers: Partial<Record<CityName, Offers>>;
+
+    у этого типа нет forEach;
+
+    в строгом режиме с noImplicitAny / noUnsafe* это выливается в ошибку вида «Unsafe call of an any typed value» (потому что TS не знает, что именно ты вызываешь).
+
+    Как правильно итерировать GroupedOffers
+    Поскольку GroupedOffers — это объект, а не Map, нужно использовать Object.entries (или Object.keys):
+
+  */
   const transformedFilteredOffers: GroupedOffersByCity[] = [];
 
   groupedOffers.forEach((offers, city) => {
